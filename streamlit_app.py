15f0cff38bc9f764d9f0cbc6c9ff7270e14b5fec
barendkruger-beta <barendkruger@gmail.com>
2026-03-29 05:55:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import sql
+import session_states
+import pages
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Initialize session states
+session_states.init()  
+
+# Initialize SQL database
+init = sql.init()
+
+# Page navigation
+app_pages = pages.Pages()
+app_pages.dyn_pages_refresh()
+pg = st.navigation(app_pages.dyn_pages | app_pages.stat_pages)
+if "page" in st.session_state:
+    if st.session_state.page is not None:
+        page = st.session_state.page
+        st.session_state.page = None
+        st.switch_page(page)                
+pg.run()
